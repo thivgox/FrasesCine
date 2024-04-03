ae13607818ea03d7e5c309ca2aa7a5d71a87831b
Thiago Moraes <thimoraesadm@gmail.com>
2024-04-03 15:34:56 -0300

    versao inicial

---
+++ src/test/java/br/com/frases/FrasesFilmes/FrasesFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.frases.FrasesFilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrasesFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
